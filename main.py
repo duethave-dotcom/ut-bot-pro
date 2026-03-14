7841665fbcc842461bd3d5f2c721edeaedf6e545
duethave-dotcom <duethave@gmail.com>
2026-03-15 02:52:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
+# هنا بنخلي السيرفر يستقبل البيانات اللي العميل هيدخلها
 @app.route('/')
-def index():
-    return "<h1>UT Bot is Online!</h1>"
+def home():
+    return """
+    <h1>MetaTrader 5 Bot System</h1>
+    <p>Status: <b>Online & Live</b></p>
+    <p>Waiting for MT5 Configuration...</p>
+    """
 
 if __name__ == "__main__":
-    # ده البورت اللي موقع Render بيفهمه
+    # ده البورت اللي Render بيشغله
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
